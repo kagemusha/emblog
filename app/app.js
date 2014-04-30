e4b184231b6173dd4d63a67717dfb128b09bddd8
kagemusha <mmadrid99@gmail.com>
2014-04-30 14:02:55 +0900

    init commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+var App = Ember.Application.extend({
+  modulePrefix: 'emblog', // TODO: loaded via config
+  Resolver: Resolver
+});
+
+loadInitializers(App, 'emblog');
+
+export default App;
