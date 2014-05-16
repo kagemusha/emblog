18d805a00f245cb84f83a85cae27c308a9590939
kagemusha <mmadrid99@gmail.com>
2014-05-16 10:18:38 +0800

    basic post page

--- app/router.js
+++ app/router.js
@@ -5,10 +5,15 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route("login");
   this.route("registration");
+
   this.resource('posts', function(){
     this.route("index", {path: "/"});
+    this.route("new");
   });
-  this.resource("post", {path: "/post/:post_id"});
+
+  this.resource("post", {path: "/post/:post_id"}, function(){
+    //edit, etc
+  });
 });
 
 export default Router;
